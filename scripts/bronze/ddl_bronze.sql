e41d260ffe4186bd976c41ab50ece56d38120133
Chandrika Chokka <51481667+DataRecipesByChandrika@users.noreply.github.com>
2026-04-14 13:04:11 +0530

    Rename columns in crm_sales_details table

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -46,10 +46,10 @@ GO
 
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num		NVARCHAR(50),
-	sle_prd_key		NVARCHAR(50),
-	sla_cust_id		INT,
+	sls_prd_key		NVARCHAR(50),
+	sls_cust_id		INT,
 	sls_order_dt	INT,
-	sla_ship_dt		INT,
+	sls_ship_dt		INT,
 	sls_due_dt		INT,
 	sls_sales		INT,
 	sls_quantity	INT,
